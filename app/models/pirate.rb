24e34a9701442478cb6ec9c7b16d34af40f04d62
Andrei Pestenariu <pclandrei@gmail.com>
2020-03-16 22:28:47 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  @@all_pirates = []
+  
+  def initialize(details)
+    @name = details[:name]
+    @weight = details[:weight]
+    @height = details[:height]  
+    @@all_pirates << self
+  end
+  
+  def self.all 
+    @@all_pirates
+  end
 end
