429a8870cbda250156057b0f37391b1e972e8668
Swishyfishie <pclandreiuk@gmail.com>
2020-03-17 00:03:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,21 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root
+    end
+
+
+    get '/new' do 
+  
+
+      erb :'pirates/new'
+    end
+
+
+    post '/pirates' do 
+
+      erb :'pirates/show'
+    end
   end
 end
